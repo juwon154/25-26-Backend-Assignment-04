58b0d7d3fbc231f11a560c4a81a19bc932957242
Park, Juwon <won030702@gmail.com>
2025-11-12 21:49:53 +0900

    refactor: @EnableJpaAuditing 어노테이션 추가

--- 박주원/JWTExample/src/main/java/com/gdg/jwtexample/JwtExampleApplication.java
+++ 박주원/JWTExample/src/main/java/com/gdg/jwtexample/JwtExampleApplication.java
@@ -3,6 +3,7 @@ package com.gdg.jwtexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JwtExampleApplication {
 
